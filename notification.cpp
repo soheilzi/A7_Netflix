c9cf774f079661082230c300a01116890958045b
soheil <soheil.zibakhsh@gmail.com>
2019-05-11 22:48:18 +0430

    updated and made notification classes and methods

---
+++ notification.cpp
@@ -0,0 +1,63 @@
+#include "notification.h"
+
+using namespace std;
+
+Notification::Notification() {
+	read = false;
+}
+
+bool Notification::is_read() {
+	return read;
+}
+
+
+Notif_from_user::Notif_from_publisher(std::string _publisher_name, int _publisher_id) : Notification() {
+	publisher_name = _publisher_name;
+	publisher_id = _publisher_id;
+}
+
+Notif_from_publisher::Notif_from_user(std::string _user_name, int _user_id, string _film_name, int _film_id) : Notification() {
+	user_name = _user_name;
+	user_id = _user_id;
+	film_name = _film_name;
+	film_id = _film_id;
+}
+
+
+Notif_reply_comment::Notif_reply_comment(std::string _publisher_name, int _publisher_id)
+	: Notif_from_publisher(_publisher_name, _publisher_id) {}
+
+Notif_new_film::Notif_new_film(std::string _publisher_name, int _publisher_id)
+	: Notif_from_publisher(_publisher_name, _publisher_id) {}
+
+
+
+Notif_movie_sale::Notif_movie_sale(std::string _user_name, int _user_id)
+	: Notif_from_user(_user_name, _user_id) {}
+
+Notif_movie_rate::Notif_movie_rate(std::string _user_name, int _user_id)
+	: Notif_from_user(_user_name, _user_id) {}
+
+Notif_movie_comment::Notif_movie_comment(std::string _user_name, int _user_id)
+	: Notif_from_user(_user_name, _user_id) {}
+
+
+string Notif_reply_comment::get_notif() {
+	return "Publisher " + publisher_name + " with id " + to_string(publisher_id) + "reply to your comment.";
+}
+
+string Notif_new_film::get_notif() {
+	return "Publisher " + publisher_name + " with id " + to_string(publisher_id) + "register new film.";
+}
+
+string Notif_movie_sale::get_notif() {
+	return "User " + user_name + " with id " + to_string(user_id) + " buy your film " + film_name + " with id " to_string(film_id);
+}
+
+string Notif_movie_rate::get_notif() {
+	return "User " + user_name + " with id " + to_string(user_id) + " rate your film " + film_name + " with id " to_string(film_id);
+}
+
+string Notif_movie_comment::get_notif() {
+	return "User " + user_name + " with id " + to_string(user_id) + " comment on your film " + film_name + " with id " to_string(film_id);
+}
---
+++ notification.h
@@ -0,0 +1,68 @@
+#ifndef __notif
+#define __notif
+
+#include <string>
+#include <iostream>
+
+class Notification {
+public:
+	Notification();
+	bool is_read();
+	virtual std::string get_notif() = 0;
+protected:
+	bool read;
+};
+
+class Notif_from_publisher : public Notification {
+public:
+	Notif_from_publisher(std::string _publisher_name, int _publisher_id);
+	virtual std::string get_notif() = 0;
+protected:
+	std::string publisher_name;
+	int publisher_id;
+};
+
+class Notif_from_user : public Notification {
+public:
+	Notif_from_user(std::string _user_name, int _user_id, std::string film_name, int film_id);
+	virtual std::string get_notif() = 0;
+protected:
+	std::string user_name;
+	std::string user_id;
+	std::string film_name;
+	int film_id;
+};
+
+class Notif_reply_comment : public Notif_from_publisher {
+public:
+	Notif_reply_comment(std::string _publisher_name, int _publisher_id);
+	virtual std::string get_notif();
+};
+
+class Notif_new_film : public Notif_from_publisher {
+public:
+	Notif_new_film(std::string _publisher_name, int _publisher_id);
+	virtual std::string get_notif();
+};
+
+class Notif_movie_sale : public Notif_from_user {
+public:
+	Notif_movie_sale(std::string _user_name, int _user_id);
+	virtual std::string get_notif();
+};
+
+class Notif_movie_rate : public Notif_from_user {
+public:
+	Notif_movie_rate(std::string _user_name, int _user_id);
+	virtual std::string get_notif();
+};
+
+class Notif_movie_comment : public Notif_from_user {
+public:
+	Notif_movie_comment(std::string _user_name, int _user_id);
+	virtual std::string get_notif();
+};
+
+
+
+#endif
